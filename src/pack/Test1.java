8abd9ac80e756b38f61c92f23681c0fa803e9e44
HP <HP@LAPTOP-06RA4R2G>
2025-02-03 19:12:52 +0530

    First Commit

---
+++ src/pack/Test1.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String hungry="yes";
+       if(hungry=="yes")
+       {
+    	   System.out.println("Eat Something....");
+       }
+       else{
+    	   System.out.println("Do work...");
+       }
+       
+	}
+}
