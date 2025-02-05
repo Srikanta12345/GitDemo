1d319d1e8ece770bf58536b69d380d7736384ad3
HP <HP@LAPTOP-06RA4R2G>
2025-02-05 20:48:16 +0530

    Added one more check

--- src/pack/Test3.java
+++ src/pack/Test3.java
@@ -5,7 +5,7 @@ public class Test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String hungry="yes";
-	       if(hungry=="yes"||hungry=="Y")
+	       if(hungry=="yes"||hungry=="Y"||hungry=="y")
 	       {
 	    	   System.out.println("Eat Something....");
 	       }
