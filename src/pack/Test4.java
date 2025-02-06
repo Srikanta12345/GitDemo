2254174a2d7f7466d306b5f5838757ef064b3c8e
Srikanta <srikantamukherjee2008@gmail.com>
2025-02-06 18:07:45 +0530

    Test4 added

---
+++ src/pack/Test4.java
@@ -0,0 +1,12 @@
+package pack;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println(" Inside Test4 Test");
+	}
+
+}
